ff8fe3864745686fbc962235d7473cb3702eaa41
michael <mpthornton86@gmail.com>
2021-01-27 16:14:49 -0700

    commit tree 453c585a2abf2d8d02c0553fb4786c387285e91e

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = time.split(":")[0];
+  if (parseInt(hour) < 12)
+    return "Good Morning"
+  if (parseInt(hour) > 17)
+    return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  let node = document.getElementById('greeting')
+  node.innerText = `${message}`
+}
